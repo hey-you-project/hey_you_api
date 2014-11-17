bf1f3d365fb40e57c3e518417e63ca88eeadd473
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-17 13:58:22 -0800

    searches the database for dots

--- routes/dot_routes.js
+++ routes/dot_routes.js
@@ -16,22 +16,22 @@ module.exports = function(app) {
 
     app.get('/api/dots', function(req, res) {
         console.log('PRINTING zone:', req.headers.zone);
-        console.log('PRINTING AND PARSING zone:', JSON.parse(req.headers.zone));
+        var zone = JSON.parse(req.headers.zone));
         /*
         {"latMax":47.61070610565,"longMin":-122.3387206914,"longMax":-122.3254213086,"latMin":47.60171189435}
         */
         //console.log('PRINTING HEADERS:', req.headers);
         //console.log(JSON.parse(req.headers.Zone));
-        res.json(req);
-//        Dot.find({latitude:{ $gt: req.body.minLat, $lt: req.body.maxLat},
-//               longitude: {$gt: req.body.minLong, $lt: req.body.maLong}},
-//              function(err, data) {
-//            if (err) {
-//                console.log(err);
-//                return res.status(500).send('there was an error');
-//            }
-//            res.json(data);
-//        });
+        //res.json(zone);
+        Dot.find({latitude:{ $gt: zone.latMin, $lt: zone.latMax},
+               longitude: {$gt: zone.longMin, $lt: zone.longMax}},
+              function(err, data) {
+            if (err) {
+                console.log(err);
+                return res.status(500).send('there was an error');
+            }
+            res.json(data);
+        });
     });
 
     app.post('/api/dots', function(req, res) {
