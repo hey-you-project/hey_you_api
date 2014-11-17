82c49c53a2c1f1d50a9dee0874cbbc5af6ca34d6
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-17 14:13:21 -0800

    lolz

--- routes/dot_routes.js
+++ routes/dot_routes.js
@@ -15,10 +15,10 @@ module.exports = function(app) {
     });
 
     app.get('/api/dots', function(req, res) {
-        var zone = JSON.parse(req.headers.zone);
         if(!req.headers.zone){
             res.status(500).send('expected zone in headers');
         }
+        var zone = JSON.parse(req.headers.zone);
         /*
         example data from zone:
         {"latMax":47.61070610565,"longMin":-122.3387206914,"longMax":-122.3254213086,"latMin":47.60171189435}
