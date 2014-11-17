4c5a0f721c43931314dad11fcd8f846d48098265
Paul Laskowski <pelaskowski@gmail.com>
2014-11-17 13:11:47 -0800

    added another test and added some dev node packages

--- test/api/dots_test.js
+++ test/api/dots_test.js
@@ -1,15 +1,19 @@
+process.env.MONGO_URL = 'mongodb://localhost/hey_you_test';
+var chai = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
 
 require('../../server');
 
 var expect = chai.expect;
+var apiBase = '';
 
 describe('basic dot CRUD', function(){
+  var dotId
   
   it('should return an array of dots (GET api/dots)')
   
-  it('should create a dot (POST api/dots)', function() {
+  it('should create a dot (POST api/dots)', function(done) {
     chai.request('http://localhost:3000')
     .post(apiBase + '/api/dots')
     //.set({jwt: jwtToken})
@@ -27,11 +31,21 @@ describe('basic dot CRUD', function(){
       expect(res.body.body).to.eql('Nice shoes!!');
       expect(res.body).to.have.property('_id');
       //expect username_id to exist in User._Id
-      id = res.body._id;
+      dotId = res.body._id;
       done();
     });
   });
   
-  it('should get an individual dot (GET api/dots/:id)')
+  it('should get an individual dot (GET api/dots/:id)', function(done) {
+    chai.request('http://localhost:3000')
+    .get(apiBase + '/api/dots/' + dotId)
+    .set({jwt: jwtToken})
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res.body.body).to.eql('Nice shoes!!');
+      expect(res.body).to.have.property('_id');
+      done();
+    });
+  });
   
 });
