463561d20c2c8cd71332bb30c8f43a78143b9932
Paul Laskowski <pelaskowski@gmail.com>
2014-11-18 10:14:45 -0800

    fixed up how the tests were sending zone header

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-jscs');
 
   grunt.initConfig({
     jshint: {
@@ -10,11 +11,18 @@ module.exports = function(grunt) {
       src: ['models/**/*.js', 'server.js', 'routes/**/*.js']
     },
 
+    jscs: {
+      src: ['models/**/*.js', 'server.js', 'routes/**/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+
     simplemocha: {
       src: ['test/api/**/*.js']
     }
   });
 
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
   grunt.registerTask('default', ['test']);
 };
