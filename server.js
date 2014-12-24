6634027d6e401380a05cb2f52d5c948ebbdce3f1
Charles Renwick <crenwick@crenwick-mac.local>
2014-12-23 20:19:42 -0800

    building a CL crawler that takes a city and adds dots to the DB

--- server.js
+++ server.js
@@ -56,3 +56,5 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
   console.log('server listening on ' + app.get('port'));
 });
+var clist = require('./lib/cl_crawl');
+clist('seattle');
