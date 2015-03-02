a7f09ce3d4249a290cd4e026406a08b00b64e974
Charles Renwick <charles.srenwick@gmail.com>
2015-03-01 17:24:26 -0800

    messing with the crawler

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URL ||
                  process.env.MONGOLAB_URI ||
                  'mongodb://localhost/hey_you_db');
 
-var dotAgeOfExpire = 172800;  //in seconds (48hrs is 172800sec, 15min is 900s)
+var dotAgeOfExpire = 172800;  //in seconds (48hrs is 172800sec, 15min is 900s) (1000 * 900)
 setInterval(function() { require('./lib/archive_old_dots')(dotAgeOfExpire); }, (1000 * 900));
 
 app.set('jwtSecret', process.env.JWT_SECRET || 'changethisordie');
